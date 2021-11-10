eb4d0806e58fd830a4abeba4e5b4d98d604e273b
Dan nguyen <danek.nguyen@gmail.com>
2021-11-10 23:23:51 +0100

    Přidejte soubory projektu.

---
+++ FilmList/Film.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmList
+{
+    class Film
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; } 
+            set { name = value; }
+        }
+
+        private string genre;
+
+        public string Genre 
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        private string releaseDate;
+
+        public string ReleaseDate
+        {
+            get { return releaseDate; }
+            set { releaseDate = value; }
+        }
+
+        private string rating;
+
+        public string Rating
+        {
+            get { return rating; }   
+            set { rating = value; } 
+        }
+    }
+}
